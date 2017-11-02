b80dc7b4a23ec07066347a9abc2cc8ba32c4e313
Gogh-office <4535292@qq.com>
2017-11-02 20:49:02 +0800

    vue project for app day1 -- component

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="heytoo">
+    <p class="dataColor">{{ msg }}</p>
+    <img src="./assets/logo.png">
+    <HelloWorld2></HelloWorld2>
+    <myooo/>
+  </div>
+</template>
+
+<script>
+import HelloWorld2 from './components/HelloWorld'
+import myooo from './components/owner-component'
+
+export default {
+  components: {
+    HelloWorld2,
+    myooo
+  },
+  data () {
+    return {
+      msg: 'App.vue组件自身的 data.msg 数据'
+    }
+  }
+}
+</script>
+
+<style>
+#heytoo {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.dataColor {
+  color: red;
+}
+.dataColor2 {
+  color: blue;
+}
+</style>
---
+++ src/components/owner-component.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="dataColor2">{{ hell }}</div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        hell: '这是个人定义的第一个组件component'
+      }
+    }
+  }
+</script>
